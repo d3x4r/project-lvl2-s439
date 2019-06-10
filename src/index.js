00b228b81ddd2ad9ead26aae1d13a86478a3930a
Dexer <norton@e1.ru>
2019-06-10 19:09:49 +0500

    Добавляет рендеринг ast дерева

--- src/index.js
+++ src/index.js
@@ -7,32 +7,67 @@ export default (beforePath, afterPath) => {
   const beforeData = readData(beforePath);
   const afterData = readData(afterPath);
 
-  console.log(buildAst(beforeData, afterData)[2].children);
+  const ast = buildAst(beforeData, afterData);
+  // console.log(ast);
 
-  const diffsWithoutNewData = Object.keys(beforeData).reduce((acc, beforeObjKey) => {
-    const objEqualPredicate = beforeData[beforeObjKey] === afterData[beforeObjKey];
+  // const str = (obj) => {
+  //   // return Object.keys(obj).map();
+  //   return `{Object.keys(obj).map()}`;
+  // };
 
-    if (_.has(afterData, beforeObjKey) && objEqualPredicate) {
-      return [`  ${beforeObjKey}: ${afterData[beforeObjKey]}`, ...acc];
-    }
+  const render = (astTree, indent = 4) => {
+    
+    return `{\n${astTree.map((element) => {
+      if (element.status === 'unchanged') {
+        if (element.type === 'node') {
+          return `${' '.repeat(indent)}${element.name}: ${render(element.children, indent + 4)}`;
+        }
+        return `${' '.repeat(indent)}${element.name}: ${element.value}`;
+      }
 
-    if (_.has(afterData, beforeObjKey) && !objEqualPredicate) {
-      return [
-        `+ ${beforeObjKey}: ${afterData[beforeObjKey]}`,
-        `- ${beforeObjKey}: ${beforeData[beforeObjKey]}`,
-        ...acc,
-      ];
-    }
-    return [`- ${beforeObjKey}: ${beforeData[beforeObjKey]}`, ...acc];
-  }, []);
+      if (element.status === 'added') {
+        if (element.type === 'node') {
+          console.log(str(element.children));
+          return `${' '.repeat(indent - 2)}+ ${element.name}: ${JSON.stringify(element.children)}`;
+        }
+        return `${' '.repeat(indent - 2)}+ ${element.name}: ${element.value}`;
+      }
 
-  const newKeysInAfterData = Object.keys(afterData).reduce((acc, afterObjKey) => {
-    if (!_.has(beforeData, afterObjKey)) {
-      return [`+ ${afterObjKey}: ${afterData[afterObjKey]}`, ...acc];
-    }
-    return acc;
-  }, []);
-  return `{\n  ${[...diffsWithoutNewData, ...newKeysInAfterData].sort(compare).join('\n  ')}\n}`;
+      if (element.status === 'deleted') {
+        if (element.type === 'node') {
+          return `${' '.repeat(indent - 2)}- ${element.name}: ${JSON.stringify(element.children)}`;
+        }
+        return `${' '.repeat(indent - 2)}- ${element.name}: ${element.value}`;
+      }
+    }).sort(compare).join('\n')}\n${' '.repeat(indent - 4)}}`;
+  };
+  console.log(render(ast));
+
+
+  // const diffsWithoutNewData = Object.keys(beforeData).reduce((acc, beforeObjKey) => {
+  //   const objEqualPredicate = beforeData[beforeObjKey] === afterData[beforeObjKey];
+
+  //   if (_.has(afterData, beforeObjKey) && objEqualPredicate) {
+  //     return [`  ${beforeObjKey}: ${afterData[beforeObjKey]}`, ...acc];
+  //   }
+
+  //   if (_.has(afterData, beforeObjKey) && !objEqualPredicate) {
+  //     return [
+  //       `+ ${beforeObjKey}: ${afterData[beforeObjKey]}`,
+  //       `- ${beforeObjKey}: ${beforeData[beforeObjKey]}`,
+  //       ...acc,
+  //     ];
+  //   }
+  //   return [`- ${beforeObjKey}: ${beforeData[beforeObjKey]}`, ...acc];
+  // }, []);
+
+  // const newKeysInAfterData = Object.keys(afterData).reduce((acc, afterObjKey) => {
+  //   if (!_.has(beforeData, afterObjKey)) {
+  //     return [`+ ${afterObjKey}: ${afterData[afterObjKey]}`, ...acc];
+  //   }
+  //   return acc;
+  // }, []);
+  // return `{\n  ${[...diffsWithoutNewData, ...newKeysInAfterData].sort(compare).join('\n  ')}\n}`;
 };
 
 // fn('/home/dexer/diffs/before.json', '/home/dexer/diffs/after.json');
