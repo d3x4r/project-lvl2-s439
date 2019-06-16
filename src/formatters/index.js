9a134e6bcf521e3057136052322b706a1ea10ccb
Dexer <norton@e1.ru>
2019-06-16 19:24:44 +0500

    Удаляет лишний модуль

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,11 +1,10 @@
 import formatToPlain from './format-to-plain';
 import formatToTree from './format-to-tree';
-import formatToJson from './format-to-json';
 
 const formatters = {
   plain: formatToPlain,
   tree: formatToTree,
-  json: formatToJson,
+  json: JSON.stringify,
 };
 
 export default (astTree, formatType) => formatters[formatType](astTree);
